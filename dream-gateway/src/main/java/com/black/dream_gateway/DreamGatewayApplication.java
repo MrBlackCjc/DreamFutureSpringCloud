f51cb889a85af7d8b8ab305f1cdfd9ed92d75ed2
jiancai.chen <jiancai.chen@ezhiyang.com>
2021-06-03 16:46:09 +0800

    Second submission.

--- dream-gateway/src/main/java/com/black/dream_gateway/DreamGatewayApplication.java
+++ dream-gateway/src/main/java/com/black/dream_gateway/DreamGatewayApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.black.dream_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DreamGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(DreamGatewayApplication.class, args);
     }
 
 }
--- dream-gateway/src/test/java/com/black/dream_gateway/DreamGatewayApplicationTests.java
+++ dream-gateway/src/test/java/com/black/dream_gateway/DreamGatewayApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.black.dream_gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class DreamGatewayApplicationTests {
 
     @Test
     void contextLoads() {
